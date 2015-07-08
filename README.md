b1c88f5652310d53f29f987a6f7a4f248fa1e449
woalk <dev@woalk.com>
2015-07-08 13:54:48 +0200

    Initial commit.
    BlinkFeed v7.0.518379.

---
+++ README.md
@@ -0,0 +1,38 @@
+#HTC BlinkFeed Authorizer
+##Xposed Module
+This [Xposed Module](http://repo.xposed.info/) removes the message
+*"Unauthorized device - Your device is not compatible with this app."*
+from [HTC BlinkFeed/Sense Home](https://play.google.com/store/apps/details?id=com.htc.launcher&hl=en).
+
+###Requirements
+This app requires Android 5 or 5.1 Lollipop and the **correct** Xposed Framework
+for [5.0](http://forum.xda-developers.com/showthread.php?t=3034811)
+or [5.1](http://forum.xda-developers.com/showthread.php?t=3072979).
+
+Furthermore, this requires obviously HTC BlinkFeed/Sense Home.
+
+Also, this Xposed Module **does not** make a really incompatible device compatible.
+It just circumvents an occurring problem with HTC's "copy protection" of BlinkFeed's Sense libraries,
+where a usually compatible, legal HTC device gets this message without justification.
+
+###Version
+The currently supported BlinkFeed/Sense Home version is
+**`7.0.518379`**,
+which is **not** the most recent Play Store version.
+
+This may be updated over time.
+
+
+###Legal
+```
+(C) 2015 Alexander "Woalk" Köster
+         http://woalk.com
+         xda-developers user woalk
+Licensed under GNU GPL v2.
+A copy of the license can be found in this repository under `LICENSE`.
+
+ABSOLUTELY NO WARRANTY.
+This application comes AS-IS.
+The USER HIMSELF is RESPONSIBLE FOR ANY DAMAGE to his software, device, ...
+Even while it is very unlikely that such a thing happens with this small app.
+```
---
+++ app/src/main/java/com/woalk/apps/xposed/htcblinkfeedauthorizer/X_Mod.java
@@ -0,0 +1,84 @@
+package com.woalk.apps.xposed.htcblinkfeedauthorizer;
+
+import android.content.Context;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * The class to be loaded by Xposed.
+ */
+public class X_Mod implements IXposedHookLoadPackage {
+    public static final String PKG_HTC_LAUNCHER = "com.htc.launcher";
+    public static final String PKG_HTC_LIB0 = "com.htc.lib0";
+    public static final String PKG_HTC_SOCIALNETWORK_UI = "com.htc.socialnetwork.common.utils.ui";
+
+    public static final String CLASS_HDK0UTIL = PKG_HTC_LIB0 + ".HDKLib0Util";
+    public static final String CLASS_BASE_ACTIVITY = PKG_HTC_SOCIALNETWORK_UI + ".BaseActivity";
+    public static final String CLASS_COMMON_MF_MAIN_ACTIVITY = PKG_HTC_SOCIALNETWORK_UI +
+            ".CommonMfMainActivity";
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals(PKG_HTC_LAUNCHER))
+            return;
+
+        XposedHelpers.findAndHookMethod(CLASS_HDK0UTIL, lpparam.classLoader, "getSenseVersion",
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        param.setResult(7.0f);
+                    }
+                });
+
+        XposedHelpers.findAndHookMethod(CLASS_HDK0UTIL, lpparam.classLoader, "isHTCDevice",
+                new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        param.setResult(true);
+                    }
+                });
+
+        XposedHelpers.findAndHookMethod(CLASS_HDK0UTIL, lpparam.classLoader, "isHEPDevice",
+                Context.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        param.setResult(Boolean.TRUE);
+                    }
+                });
+
+        XposedHelpers.findAndHookMethod(CLASS_HDK0UTIL, lpparam.classLoader, "isODMDevice",
+                Context.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        param.setResult(Boolean.FALSE);
+                    }
+                });
+
+        XposedHelpers.findAndHookMethod(CLASS_HDK0UTIL, lpparam.classLoader, "isStockUIDevice",
+                Context.class, new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        param.setResult(Boolean.FALSE);
+                    }
+                });
+
+        XposedHelpers.findAndHookMethod(CLASS_BASE_ACTIVITY, lpparam.classLoader,
+                "checkCompatibility", new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        param.setResult(true);
+                    }
+                });
+
+        XposedHelpers.findAndHookMethod(CLASS_COMMON_MF_MAIN_ACTIVITY, lpparam.classLoader,
+                "checkCompatibility", new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                        param.setResult(true);
+                    }
+                });
+    }
+}
