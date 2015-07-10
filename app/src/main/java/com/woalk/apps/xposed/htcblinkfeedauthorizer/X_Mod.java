c347d9c859ef76e1a5b5414ed828b639c5904689
woalk <dev@woalk.com>
2015-07-10 15:56:47 +0200

    Added Instagram methods. (test/to-do!)
    First try to work on #1.
    
    Demomstration of my attempt to include Instagram's BlinkFeed add-in into the module.
    This *does not work* currently.
    
    The method "d()" equals to the method "checkCompatibility()" in HTC Launcher, but it isn't called at any time (the Log messages aren't there).
    This has to be related to the way BlinkFeed calls its add-ins' methods and Activities, because the method "d()" is th
    e *only* place where the DEVICE_NOT_COMPATIBLE Intent is started from Instagram.
    
    So, add-ins are a larger to-do.

--- app/src/main/java/com/woalk/apps/xposed/htcblinkfeedauthorizer/X_Mod.java
+++ app/src/main/java/com/woalk/apps/xposed/htcblinkfeedauthorizer/X_Mod.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
@@ -15,10 +16,16 @@ public class X_Mod implements IXposedHookLoadPackage {
     public static final String PKG_HTC_LIB0 = "com.htc.lib0";
     public static final String PKG_HTC_SOCIALNETWORK_UI = "com.htc.socialnetwork.common.utils.ui";
 
+    public static final String PKG_HTC_INSTAGRAM = "com.htc.socialnetwork.instagram";
+    public static final String PKG_HTC_LIB2 = "com.htc.lib2";
+
     public static final String CLASS_HDK0UTIL = PKG_HTC_LIB0 + ".HDKLib0Util";
     public static final String CLASS_BASE_ACTIVITY = PKG_HTC_SOCIALNETWORK_UI + ".BaseActivity";
     public static final String CLASS_COMMON_MF_MAIN_ACTIVITY = PKG_HTC_SOCIALNETWORK_UI +
             ".CommonMfMainActivity";
+
+    public static final String CLASS_INSTAGRAM_ACTIVITY = PKG_HTC_INSTAGRAM + ".InstagramActivity";
+    public static final String CLASS_INSTAGRAM_LIB2_A = PKG_HTC_LIB2 + ".a";
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
@@ -78,6 +85,33 @@ public class X_Mod implements IXposedHookLoadPackage {
                             param.setResult(true);
                         }
                     });
-        }
+        } else if (lpparam.packageName.equals(PKG_HTC_INSTAGRAM)) {
+            XposedHelpers.findAndHookMethod(CLASS_INSTAGRAM_LIB2_A, lpparam.classLoader, "a",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(7.0f);
+                            XposedBridge.log("Exec " + CLASS_INSTAGRAM_LIB2_A + ".a()");
+                        }
+                    });
+
+            XposedHelpers.findAndHookMethod(CLASS_INSTAGRAM_LIB2_A, lpparam.classLoader, "b",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(true);
+                            XposedBridge.log("Exec " + CLASS_INSTAGRAM_LIB2_A + ".b()");
+                        }
+                    });
+
+            XposedHelpers.findAndHookMethod(CLASS_INSTAGRAM_ACTIVITY, lpparam.classLoader, "d",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(true);
+                            XposedBridge.log("Exec " + CLASS_INSTAGRAM_ACTIVITY + ".d()");
+                        }
+                    });
+        } else
     }
 }
