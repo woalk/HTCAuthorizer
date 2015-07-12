eb1175f3b34249e8212884d942e65d8c8dd017e9
d8ahazard <d8ahazard@gmail.com>
2015-07-12 09:28:47 -0500

    Move "common" HTC lib0 checks to shared section
    Begin modularizing checks, adding unique ones for packages in new
    sections.

--- app/src/main/java/com/woalk/apps/xposed/htcblinkfeedauthorizer/X_Mod.java
+++ app/src/main/java/com/woalk/apps/xposed/htcblinkfeedauthorizer/X_Mod.java
@@ -21,16 +21,26 @@ public class X_Mod implements IXposedHookLoadPackage {
     public static final String CLASS_BASE_ACTIVITY = PKG_HTC_SOCIALNETWORK_UI + ".BaseActivity";
     public static final String CLASS_COMMON_MF_MAIN_ACTIVITY = PKG_HTC_SOCIALNETWORK_UI +
             ".CommonMfMainActivity";
-
     public static final String CLASS_3DSCENE = "com.htc.sunny2.frameworks.base.widgets.SunnyScene";
+	
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        if (lpparam.packageName.equals(PKG_HTC_LAUNCHER)) {
+		// First section contains common checks found in all HTC Apps
+		// Need to see if OR statements are best, or if we can just check for com.htc.* apps
+        if (lpparam.packageName.equals(PKG_HTC_LAUNCHER) || lpparam.packageName.equals(PKG_HTC_GALLERY)) {
             XposedHelpers.findAndHookMethod(CLASS_HDK0UTIL, lpparam.classLoader, "getSenseVersion",
                     new XC_MethodHook() {
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                             param.setResult(7.0f);
+                        }
+                    });
+					
+			XposedHelpers.findAndHookMethod(CLASS_HDK0UTIL, lpparam.classLoader, "getHDKBaseVersion",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(6.0f); //Guessing at this value, need to investigate
                         }
                     });
 
@@ -41,6 +51,26 @@ public class X_Mod implements IXposedHookLoadPackage {
                             param.setResult(true);
                         }
                     });
+					
+			XposedHelpers.findAndHookMethod(CLASS_HDK0UTIL, lpparam.classLoader, "isStockUIDevice",
+                    Context.class, new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(Boolean.FALSE);
+                        }
+                    });
+				//I suspect this is critical
+				XposedHelpers.findAndHookMethod(CLASS_HDK0UTIL, lpparam.classLoader, "isHDKLib3SupportedInDevice",
+                    Context.class, new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(true);
+                        }
+                    });
+
+		}
+		// Move application-specific checks here
+		if (lpparam.packageName.equals(PKG_HTC_LAUNCHER)) {
 
             XposedHelpers.findAndHookMethod(CLASS_HDK0UTIL, lpparam.classLoader, "isHEPDevice",
                     Context.class, new XC_MethodHook() {
@@ -58,14 +88,7 @@ public class X_Mod implements IXposedHookLoadPackage {
                         }
                     });
 
-            XposedHelpers.findAndHookMethod(CLASS_HDK0UTIL, lpparam.classLoader, "isStockUIDevice",
-                    Context.class, new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            param.setResult(Boolean.FALSE);
-                        }
-                    });
-
+            
             XposedHelpers.findAndHookMethod(CLASS_BASE_ACTIVITY, lpparam.classLoader,
                     "checkCompatibility", new XC_MethodHook() {
                         @Override
@@ -82,8 +105,9 @@ public class X_Mod implements IXposedHookLoadPackage {
                         }
                     });
         }
-		
+		//Gallery Checks
 		if (lpparam.packageName.equals(PKG_HTC_GALLERY)) {
+			//This check enables duo fx - tie to a toggle in UI
         XposedHelpers.findAndHookMethod(CLASS_3DSCENE, lpparam.classLoader,
                 "enable3dScene", new XC_MethodHook() {
                         @Override
