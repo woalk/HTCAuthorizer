578441e94469cb2760b77019ab8a7790cdea7803
d8ahazard <d8ahazard@gmail.com>
2015-07-12 10:48:11 -0500

    Wtf am I doing wrong?

--- app/src/main/java/com/woalk/apps/xposed/htcblinkfeedauthorizer/X_Mod.java
+++ app/src/main/java/com/woalk/apps/xposed/htcblinkfeedauthorizer/X_Mod.java
@@ -15,7 +15,8 @@ public class X_Mod implements IXposedHookLoadPackage {
 	public static final String PKG_HTC_GALLERY = "com.htc.album";
 	public static final String PKG_HTC_CAMERA = "com.htc.camera";
 	public static final String PKG_HTC_LIB0 = "com.htc.lib0";
-    public static final String PKG_HTC_SOCIALNETWORK_UI = "com.htc.socialnetwork.common.utils.ui";
+    public static final String PKG_HTC_LIB2 = "com.htc.lib2.Hms";
+	public static final String PKG_HTC_SOCIALNETWORK_UI = "com.htc.socialnetwork.common.utils.ui";
 
     public static final String CLASS_HDK0UTIL = PKG_HTC_LIB0 + ".HDKLib0Util";
     public static final String CLASS_BASE_ACTIVITY = PKG_HTC_SOCIALNETWORK_UI + ".BaseActivity";
@@ -51,6 +52,14 @@ public class X_Mod implements IXposedHookLoadPackage {
                             param.setResult(true);
                         }
                     });
+			
+			XposedHelpers.findAndHookMethod(PKG_HTC_LIB2, lpparam.classLoader, "isHTCDevice",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(true);
+                        }
+                    });
 					
 			XposedHelpers.findAndHookMethod(CLASS_HDK0UTIL, lpparam.classLoader, "isStockUIDevice",
                     Context.class, new XC_MethodHook() {
@@ -70,7 +79,7 @@ public class X_Mod implements IXposedHookLoadPackage {
 
 		}
 		// Move application-specific checks here
-		if (lpparam.packageName.equals(PKG_HTC_LAUNCHER)) {
+		else if (lpparam.packageName.equals(PKG_HTC_LAUNCHER)) {
 
             XposedHelpers.findAndHookMethod(CLASS_HDK0UTIL, lpparam.classLoader, "isHEPDevice",
                     Context.class, new XC_MethodHook() {
@@ -106,9 +115,9 @@ public class X_Mod implements IXposedHookLoadPackage {
                     });
         }
 		//Gallery Checks
-		if (lpparam.packageName.equals(PKG_HTC_GALLERY)) {
+		else if (lpparam.packageName.equals(PKG_HTC_GALLERY)) {
 			//This check enables duo fx - tie to a toggle in UI
-        XposedHelpers.findAndHookMethod(CLASS_3DSCENE, lpparam.classLoader,
+			XposedHelpers.findAndHookMethod(CLASS_3DSCENE, lpparam.classLoader,
                 "enable3dScene", new XC_MethodHook() {
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
