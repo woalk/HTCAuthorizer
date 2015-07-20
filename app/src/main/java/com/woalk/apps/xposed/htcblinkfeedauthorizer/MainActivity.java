248a7500090b7e4dff7436d3f50808cf8cc87aaa
woalk <dev@woalk.com>
2015-07-20 22:23:52 +0200

    Made Toolbar style more HTC-ish.
    Added an own Activity layout with a custom Toolbar with a custom Theme.

--- app/src/main/java/com/woalk/apps/xposed/htcblinkfeedauthorizer/MainActivity.java
+++ app/src/main/java/com/woalk/apps/xposed/htcblinkfeedauthorizer/MainActivity.java
@@ -2,13 +2,23 @@ package com.woalk.apps.xposed.htcblinkfeedauthorizer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getFragmentManager().beginTransaction().replace(android.R.id.content, new
-                MainPreferenceFragment()).commit();
+        setContentView(R.layout.activity_replaceable);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        TextView title = (TextView) toolbar.findViewById(android.R.id.title);
+        setActionBar(toolbar);
+        title.setText(toolbar.getTitle());
+        getActionBar().setTitle(null);
+
+        getFragmentManager().beginTransaction().replace(android.R.id.widget_frame,
+                new MainPreferenceFragment()).commit();
     }
 }
