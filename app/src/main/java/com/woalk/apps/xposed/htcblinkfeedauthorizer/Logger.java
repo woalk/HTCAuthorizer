1d3c9cdb324864d01b171051e79afba0e3707079
woalk <dev@woalk.com>
2015-07-30 21:42:02 +0200

    Added class name to logged hook.

--- app/src/main/java/com/woalk/apps/xposed/htcblinkfeedauthorizer/Logger.java
+++ app/src/main/java/com/woalk/apps/xposed/htcblinkfeedauthorizer/Logger.java
@@ -74,12 +74,14 @@ public class Logger {
     }
 
     public static void logHook(XC_MethodHook.MethodHookParam param) {
-        v("Method hook %s executed, pre-result=%s;args=%s", param.method.getName(),
+        v("Method hook %s/.%s executed, pre-result=%s;args=%s",
+                param.method.getDeclaringClass().getName(), param.method.getName(),
                 param.getResult(), getArrayString(param.args));
     }
 
     public static void logHookAfter(XC_MethodHook.MethodHookParam param) {
-        v("Method hook %s ended, post-result=%s;args=%s", param.method.getName(),
+        v("Method hook %s/.%s executed, post-result=%s;args=%s",
+                param.method.getDeclaringClass().getName(), param.method.getName(),
                 param.getResult(), getArrayString(param.args));
     }
 
