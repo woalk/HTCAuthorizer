5e9c06555cea68ee781c8771aee30b4ad561b6cf
d8ahazard <d8ahazard@gmail.com>
2015-08-10 19:12:02 -0500

    Update quickbuild tool
    Add error checking to save time.

--- qb.bat
+++ qb.bat
@@ -1,5 +1,10 @@
 @echo off
 call gradlew build
+IF NOT ERRORLEVEL 0 GOTO errorHandling
 adb install -r .\app\build\outputs\apk\app-debug.apk
 timeout /t 5
 adb reboot
+goto EOF
+:errorHandling
+echo Error compiling, press any key to exit.
+pause
