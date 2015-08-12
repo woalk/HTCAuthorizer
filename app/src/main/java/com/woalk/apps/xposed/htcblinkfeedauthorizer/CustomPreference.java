8a89496f8a537f49adf59db5092af957bed3b958
d8ahazard <d8ahazard@gmail.com>
2015-08-12 15:59:44 -0500

    One more.

--- app/src/main/java/com/woalk/apps/xposed/htcblinkfeedauthorizer/CustomPreference.java
+++ app/src/main/java/com/woalk/apps/xposed/htcblinkfeedauthorizer/CustomPreference.java
@@ -1,23 +1,44 @@
 package com.woalk.apps.xposed.htcblinkfeedauthorizer;
 
 import android.content.Context;
+import android.graphics.drawable.ShapeDrawable;
 import android.preference.Preference;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageView;
 
 public class CustomPreference extends Preference {
+    public Integer myTheme = null;
+
+    public CustomPreference(Context context) {
+        super(context);
+    }
+
     public CustomPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    @Override
-    protected void onBindView(View rootView) {
-        super.onBindView(rootView);
-
-        View myView = rootView.findViewById(R.id.button);
-        //Stop here for the night.
-        //myView.getDrawable().setColorFilter();
-        Logger.d("CustomPreference: It works!" + myView);
-        // do something with myView
+    public CustomPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
     }
+    @Override
+    public void onBindView(View rootView) {
+        super.onBindView(rootView);
+        ImageView myView = (ImageView) rootView.findViewById(R.id.button);
+
+        if ((myView != null) && (myTheme != null)) {
+            Logger.d("CustomPreference: It works!" + " " + myView + " and color of " + myTheme);
+            myView.setBackgroundColor(myTheme);
+        } else {
+            Logger.d("CustomPreference: Close, but no cigar!" + " " + myView + " and color of " + myTheme);
+        }
+
+    }
+
+
+    public void setMyColor(int themecolor) {
+        myTheme = themecolor;
+        Logger.d("CustomPreference: Color passed " + myTheme);
+    }
+
 }
